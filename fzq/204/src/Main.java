2550bf3a40eec56c168c5bcca5be2b6651e1fb54
fanzeqiu <51195100007@stu.ecnu.edu.cn>
2020-12-03 13:58:38 +0800

    204

---
+++ fzq/204/src/Main.java
@@ -0,0 +1,7 @@
+public class Main {
+
+    public static void main(String[] args) {
+        Solution solution = new Solution();
+        System.out.println(solution.countPrimes(499979));
+    }
+}
---
+++ fzq/204/src/Solution.java
@@ -0,0 +1,18 @@
+class Solution {
+    public int countPrimes(int n) {
+        int count = 0;
+        for (int i = 2; i < n; i++) {
+            if (isPrimes(i))
+                count++;
+        }
+        return count;
+    }
+
+    private boolean isPrimes(int n) {
+        for (int i = 2; i * i <= n; i++) {
+            if (n%i == 0)
+                return false;
+        }
+        return true;
+    }
+}
