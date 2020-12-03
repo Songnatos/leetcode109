2afa8b9ac67ff79da0ffa7b628f5a5afab9711c1
shanlumin <51195100045@stu.ecnu.edu.cn>
2020-12-03 19:00:58 +0800

    204

--- slm/easy/204S/src/Main.java
+++ slm/easy/204S/src/Main.java
@@ -2,6 +2,7 @@ public class Main {
 
     public static void main(String[] args) {
         Solution solution = new Solution();
+        System.out.println(solution.countPrimes(499979));
         System.out.println("Hello World!");
     }
 }
