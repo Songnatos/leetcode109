7eeff362ccaddd5f5423baa0da9149153b694227
fanzeqiu <zeno_van@163.com>
2020-12-07 08:53:43 +0800

    861 mid

---
+++ fzq/861f/src/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+
+    public static void main(String[] args) {
+        Solution solution = new Solution();
+        int[][] a = {{0,0,1,1}, {1,0,1,0}, {1,1,0,0}};
+        System.out.println(solution.matrixScore(a));
+    }
+}
