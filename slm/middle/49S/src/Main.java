496604228e6be93bfc5029771edac81344a34d55
slm <979255061@qq.com>
2020-12-14 21:55:48 +0800

    12.14

---
+++ slm/middle/49S/src/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+
+    public static void main(String[] args) {
+        String[] a= {"eat", "tea", "tan", "ate", "nat", "bat"};
+        Solution solution = new Solution();
+        solution.groupAnagrams(a);
+        System.out.println("Hello World!");
+    }
+}
