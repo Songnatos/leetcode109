3e545c1ad9fb4a3ad8b3bb9b1b3121e37b642739
yc <bling_hong@163.com>
2020-12-16 11:18:32 +0800

    290-单词规律

---
+++ hyc/290h/src/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+
+    public static void main(String[] args) {
+        Solution solution = new Solution();
+        System.out.println(solution.wordPattern("abba",
+                "dog cat cat dog"));
+    }
+
+}
