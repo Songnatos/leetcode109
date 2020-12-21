6f90f346374f83c355d8d2852e6600bb3097f5c3
fanzeqiu <zeno_van@163.com>
2020-12-21 11:16:07 +0800

    动态规划 746

---
+++ fzq/746f/src/Main.java
@@ -0,0 +1,7 @@
+public class Main {
+
+    public static void main(String[] args) {
+        Solution solution = new Solution();
+        System.out.println(solution.minCostClimbingStairs(new int[]{1, 100, 1, 1, 1, 100, 1, 1, 100, 1}));
+    }
+}
