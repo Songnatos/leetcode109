9052789fcfd0323544969172777e351fe70f0560
fanzeqiu <zeno_van@163.com>
2020-12-22 09:55:59 +0800

    103

---
+++ fzq/103f/src/Main.java
@@ -0,0 +1,12 @@
+public class Main {
+
+    public static void main(String[] args) {
+        Solution solution = new Solution();
+        TreeNode root = new TreeNode(3);
+        root.left = new TreeNode(9);
+        root.right = new TreeNode(20);
+        root.right.left = new TreeNode(15);
+        root.right.right = new TreeNode(7);
+        System.out.println(solution.zigzagLevelOrder(root));
+    }
+}
