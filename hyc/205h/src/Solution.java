e7de55863bdcbb445eee6843a0de4546d1d832b7
yc <bling_hong@163.com>
2020-12-27 13:44:36 +0800

    205.同构字符串

---
+++ hyc/205h/src/Solution.java
@@ -0,0 +1,20 @@
+import java.util.HashMap;
+import java.util.Map;
+
+class Solution {
+    public boolean isIsomorphic(String s, String t) {
+        Map<Character, Character> map1 = new HashMap<>();
+        Map<Character, Character> map2 = new HashMap<>();
+
+        for (int i = 0; i < s.length(); i ++) {
+            char x = s.charAt(i);
+            char y = t.charAt(i);
+            if ((map1.containsKey(x) && map1.get(x) != y) || (map2.containsKey(y) && map2.get(y) != x)) {
+                return false;
+            }
+            map1.put(x, y);
+            map2.put(y, x);
+        }
+        return true;
+    }
+}
