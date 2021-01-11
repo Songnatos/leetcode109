1d47604a2f1f1af7064b4d45e625a92f1d6859b8
slm <979255061@qq.com>
2021-01-11 21:24:39 +0800

    1.11

--- slm/UnionFind/1201S/src/Solution.java
+++ slm/UnionFind/1201S/src/Solution.java
@@ -1,6 +1,9 @@
 import jdk.nashorn.internal.ir.UnaryNode;
 
 import java.security.PublicKey;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashMap;
 import java.util.List;
 
 class Solution {
@@ -8,20 +11,33 @@ class Solution {
     int n=s.length();
     char[] temp=s.toCharArray();
     UnionFind unionFind= new UnionFind(n);
+        HashMap<Integer, List<Integer>> map = new HashMap<>();
     for(List<Integer> pair:pairs){
         unionFind.union(pair.get(0),pair.get(1));
     }
-    for(int i=n-1;i>0;i--){
-        for(int j=i-1;j>=0;j--){
-            if(unionFind.find(i)==unionFind.find(j)&&temp[i]<temp[j]){
-                char t=temp[i];
-                temp[i]=temp[j];
-                temp[j]=t;
-            }
-        }
+    for(int i=0;i<n;i++){
+        int key=unionFind.find(i);
+        map.computeIfAbsent(key,unused -> new ArrayList<>()).add(i);
     }
-    return String.valueOf(temp);
+    StringBuilder res = new StringBuilder(s);
+    for( List<Integer> list :map.values()){
+        if(list.size()>1){
+            sort(res,list);
+        }
 
+    }
+    return res.toString();
+
+    }
+    public void sort(StringBuilder s,List<Integer> list){
+        char[] arrar=new char[list.size()];
+        for(int i=0;i<list.size();i++){
+            arrar[i]=s.charAt(list.get(i));
+        }
+        Arrays.sort(arrar);
+        for(int i=0;i<list.size();i++){
+            s.setCharAt(list.get(i),arrar[i]);
+        }
     }
     public  class UnionFind{
         private int[] parent;
