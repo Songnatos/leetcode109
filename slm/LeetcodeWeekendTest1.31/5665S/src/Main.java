8868f0ad719e579c39aaae68acc02faa71225de8
slm <979255061@qq.com>
2021-01-31 15:32:06 +0800

    1.31

---
+++ slm/LeetcodeWeekendTest1.31/5665S/src/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+
+    public static void main(String[] args) {
+        Solution solution = new Solution();
+        int[][] arr={{4,-2},{1,4},{-3,1}};
+        solution.restoreArray(arr);
+        System.out.println("Hello World!");
+    }
+}
