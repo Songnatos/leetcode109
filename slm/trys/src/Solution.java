8eface708dc053e7836179f778749eccaa9d8e79
slm <979255061@qq.com>
2021-03-03 09:40:31 +0800

    3.2

--- slm/trys/src/Solution.java
+++ slm/trys/src/Solution.java
@@ -1,32 +1,10 @@
-import java.util.Arrays;
-import java.util.Comparator;
-import java.util.PriorityQueue;
-
 class Solution {
-    public int maximumScore(int a, int b, int c) {
-        PriorityQueue<Integer> res=new PriorityQueue<>((x,y)->y-x);
-        res.add(a);res.add(b);res.add(c);
-        int count=0;
-        int sum=0;
-        for(Integer num:res){
-            if(num<=0){
-                count++;
-            }
-        }
-        while(count<2){
-            count=0;
-            sum++;
-            int temp1=res.poll();
-            int temp2=res.poll();
-            res.add(--temp1);
-            res.add(--temp2);
-            for(Integer num:res){
-                if(num<=0){
-                    count++;
-                }
-            }
-        }
-        return sum;
-
+    public int[] countBits(int num) {
+    int[] res=new int[num+1];
+    int level=1;
+    res[0]=0;res[1]=1;
+    for(int i=1;i<num+1;i++){
+        if(i/Math.oie==0)
+    }
     }
 }
